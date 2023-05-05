9170a7c6cb7d3d7442d5057d46d501d91fc4f4d9
Paweł Dowgiało <pawel.dowgialo@cyfroweszkoly.pl>
2023-05-05 17:20:35 +0200

    pierwszy

---
+++ kalendarz/App.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+
+export default function App() {
+  const [selectedDate, setSelectedDate] = useState('');
+  const [aktualnaData, setAktualnaData] = useState('');
+
+  useEffect(() => {
+    const d = new Date();
+    const year = d.getFullYear();
+    const month = (d.getMonth() + 1).toString().padStart(2, '0');
+    const day = d.getDate().toString().padStart(2, '0');
+    setAktualnaData(`${year}-${month}-${day}`);
+  }, []);
+
+  const wydarzenia = {
+    [aktualnaData]: [{ opis: 'Takie te' }],
+    '2023-04-20': [{ opis: 'urodziny Mścisława' }]
+  };
+
+  function selectedDateHandler(day) {
+    setSelectedDate(day.dateString);
+  }
+
+  function renderPustaData() {
+    return (
+      <View>
+        <Text>tak</Text>
+      </View>
+    );
+  }
+
+  function renderWydarzenia(wyd) {
+    return (
+      <View>
+        <Text>{wyd.opis}</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Agenda
+        items={wydarzenia}
+        renderItem={renderWydarzenia}
+        renderEmptyData={renderPustaData}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: 600,
+    backgroundColor: '#fff'
+  },
+});
